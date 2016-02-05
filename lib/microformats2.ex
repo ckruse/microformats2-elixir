fb7f97e4290f301d3910f88c7edeaf818dc10e27
Christian Kruse <cjk@defunct.ch>
2016-02-06 00:04:13 +0100

    ref: some refactoring

--- lib/microformats2.ex
+++ lib/microformats2.ex
@@ -17,21 +17,35 @@ defmodule Microformats2 do
         rel = Floki.attribute(element, "rel") |> List.first |> String.split(" ", trim: true)
         url = Floki.attribute(element, "href") |> List.first # TODO convert to absolute URL
 
-        n_map = Enum.reduce(rel, acc, fn(rel, map) ->
-          if map[:rels][rel] == nil do
-            Map.put(map, :rels, Map.put(map[:rels], rel, [url]))
-          else
-            Map.put(map, :rels, Map.put(map[:rels], rel, Enum.uniq(map[:rels][rel] ++ [url])))
-          end
-        end)
-
-        if n_map[:rel_urls][url] == nil do
-          Map.put(n_map, :rel_urls, Map.put(n_map[:rel_urls], url, rel))
-        else
-          Map.put(n_map, :rel_urls, Map.put(n_map[:rel_urls], url, Enum.uniq(n_map[:rel_urls][url] ++ rel)))
-        end
+        acc |>
+          save_urls_by_rels(rel, url) |>
+          save_rels_by_urls(rel, url)
       end)
 
     link_rels
+  end
+
+  defp save_urls_by_rels(map, rel, url) do
+    Enum.reduce(rel, map, fn(rel, nmap) ->
+      if nmap[:rels][rel] == nil do
+        Map.put(nmap, :rels,
+                Map.put(nmap[:rels], rel, [url]))
+      else
+        Map.put(nmap, :rels,
+                Map.put(nmap[:rels], rel,
+                        Enum.uniq(nmap[:rels][rel] ++ [url])))
+      end
+    end)
+  end
+
+  defp save_rels_by_urls(map, rel, url) do
+    if map[:rel_urls][url] == nil do
+      Map.put(map, :rel_urls,
+              Map.put(map[:rel_urls], url, %{rels: rel}))
+    else
+      Map.put(map, :rel_urls,
+              Map.put(map[:rel_urls], url,
+                      %{rels: Enum.uniq(map[:rel_urls][url][:rels] ++ rel)}))
+    end
   end
 end
