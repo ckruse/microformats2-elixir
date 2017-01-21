35072f749f631710f8842ba25cdbb6b793936e0f
Christian Kruse <cjk@defunct.ch>
2017-01-21 14:51:36 +0100

    version push

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule Microformats2.Mixfile do
 
   def project do
     [app: :microformats2,
-     version: "0.0.6",
+     version: "0.0.7",
      elixir: "~> 1.3",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
