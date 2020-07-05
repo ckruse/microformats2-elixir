60779eea5f7360eb5fa5befa379c8b73ebf6a638
Christian Kruse <cjk@defunct.ch>
2020-07-05 08:17:41 +0200

    feat: h-event test suite

---
+++ test/suite/h_event_test.exs
@@ -0,0 +1,53 @@
+defmodule Microformats2.Suite.HEventTest do
+  use ExUnit.Case
+
+  import Microformats2.TestUtils
+
+  # TODO: fails because of value class pattern
+  # test "ampm" do
+  #   {html, json} = suite_document("h-event/ampm")
+  #   assert Microformats2.parse(html, "http://example.com", atomize_keys: false, underscore_keys: false) == json
+  # end
+
+  test "attendees" do
+    {html, json} = suite_document("h-event/attendees")
+    assert Microformats2.parse(html, "http://example.com", atomize_keys: false, underscore_keys: false) == json
+  end
+
+  test "combining" do
+    {html, json} = suite_document("h-event/combining")
+    assert Microformats2.parse(html, "http://example.com", atomize_keys: false, underscore_keys: false) == json
+  end
+
+  # TODO: fails because of value class pattern
+  # test "concatenate" do
+  #   {html, json} = suite_document("h-event/concatenate")
+  #   assert Microformats2.parse(html, "http://example.com", atomize_keys: false, underscore_keys: false) == json
+  # end
+
+  test "dates" do
+    {html, json} = suite_document("h-event/dates")
+    assert Microformats2.parse(html, "http://example.com", atomize_keys: false, underscore_keys: false) == json
+  end
+
+  # TODO: fails because of value class pattern
+  # test "dt-property" do
+  #   {html, json} = suite_document("h-event/dt-property")
+  #   assert Microformats2.parse(html, "http://example.com", atomize_keys: false, underscore_keys: false) == json
+  # end
+
+  test "justahyperlink" do
+    {html, json} = suite_document("h-event/justahyperlink")
+    assert Microformats2.parse(html, "http://example.com", atomize_keys: false, underscore_keys: false) == json
+  end
+
+  test "justaname" do
+    {html, json} = suite_document("h-event/justaname")
+    assert Microformats2.parse(html, "http://example.com", atomize_keys: false, underscore_keys: false) == json
+  end
+
+  test "time" do
+    {html, json} = suite_document("h-event/justaname")
+    assert Microformats2.parse(html, "http://example.com", atomize_keys: false, underscore_keys: false) == json
+  end
+end
