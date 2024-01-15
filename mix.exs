9d9d4ce961b0d730208ad0a11099f89f2d41cd28
Christian Kruse <cjk@defunct.ch>
2024-01-15 07:10:51 +0100

    version 1.0.1

--- mix.exs
+++ mix.exs
@@ -2,7 +2,7 @@ defmodule Microformats2.Mixfile do
   use Mix.Project
 
   @source_url "https://github.com/ckruse/microformats2-elixir"
-  @version "1.0.0"
+  @version "1.0.1"
 
   def project do
     [
